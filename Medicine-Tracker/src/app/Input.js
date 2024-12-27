ac9783e48b65a6da7b82a72e1aa7788a979704b5
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-27 18:26:35 -0500

    Renamed TextBox to Input

--- Medicine-Tracker/src/app/Input.js
+++ Medicine-Tracker/src/app/Input.js
@@ -1,4 +1,4 @@
-export default function TextBox() {
+export default function Input() {
     return (
         <input className="border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
         type="text"></input>
--- Medicine-Tracker/src/app/page.js
+++ Medicine-Tracker/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import TextBox from "./TextBox";
+import Input from "./Input";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -8,15 +8,15 @@ export default function Home() {
         <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Name of medication: 
-            <TextBox></TextBox>
+            <Input></Input>
           </li>
           <li className="mb-2">
             How much do you take in the morning?
-            <TextBox></TextBox>
+            <Input></Input>
           </li>
           <li>
             How much do you take at night?
-            <TextBox></TextBox>
+            <Input></Input>
           </li>
         </ul>
 
