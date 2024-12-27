f31f0251e6f1d4ef21d25d5eafe57e9b8782effc
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-27 18:37:36 -0500

    attempted to recieve props and change element

--- Medicine-Tracker/src/app/Input.js
+++ Medicine-Tracker/src/app/Input.js
@@ -1,6 +1,17 @@
-export default function Input() {
-    return (
-        <input className="border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
-        type="text"></input>
-    )
+export default function Input(InputType) {
+    console.log(InputType)
+    if(InputType.type === "text")
+    {
+        return (
+            <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+            type="text"></input>
+        )
+    }
+    else
+    {
+        return (
+            <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+            type="number"></input>
+        )
+    }
 }
