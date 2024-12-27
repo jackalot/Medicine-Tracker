15a5c7f3d0b9bed9a1101f43ec136836201984ac
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-27 18:07:03 -0500

    found some styling online for the boxes

--- Medicine-Tracker/src/app/TextBox.js
+++ Medicine-Tracker/src/app/TextBox.js
@@ -1,5 +1,6 @@
 export default function TextBox() {
     return (
-        <input type="text"></input>
+        <input className="border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+        type="text"></input>
     )
 }
