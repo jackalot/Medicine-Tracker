600aab3b626e1d7610eb0ccbe5d08d01f1fde81a
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 08:53:22 -0500

    created changeParentState in Inpute, now calling it

--- Medicine-Tracker/src/app/Input.js
+++ Medicine-Tracker/src/app/Input.js
@@ -1,7 +1,8 @@
-export default function Input({InputType}) {
+export default function Input({InputType, changeParentState}) {
     console.log(InputType)
     return (
         <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
-        type={InputType}></input>
+        type={InputType}
+        onChange={changeParentState}></input>
     )
 }
