9528542c807158ea88b08a5d2c94e7314d4db85e
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 10:29:54 -0500

    created a basic function for the popup

---
+++ Medicine-Tracker/src/app/popup.js
@@ -0,0 +1,6 @@
+export default function Popup() {
+    return (
+        <div className="absolute z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          THIS OVERLAPS ALL  </div>
+    )
+}
