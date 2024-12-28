98d59d2337a1a84d14ef794717536d92894453bd
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 09:25:57 -0500

    added some psuedocode for the function

--- Medicine-Tracker/src/app/page.js
+++ Medicine-Tracker/src/app/page.js
@@ -20,7 +20,23 @@ export default function Home() {
     SetTotalPillAmount(e.target.value)
   }
   function CalculatePills() {
-
+    /*
+    say we have 1 in the morning 4 at night\
+    40 in total pills.
+    totalPills
+    morningTaken
+    nightTaken
+    while(totalPills > 0)
+    {
+      totalPills -= morning dosage
+      morningTaken += morning dosage
+      if(on this iteration, totalPills is still > 0)
+      {
+        totalPills -= night dosage
+        nightTaken += night dosage
+      }
+    }
+     */
   }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
