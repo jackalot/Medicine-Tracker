7e811db4bbc5d343f9d155c77d0ac79896d7f683
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 08:37:53 -0500

    Fixed props, removed if else statement

--- Medicine-Tracker/src/app/Input.js
+++ Medicine-Tracker/src/app/Input.js
@@ -1,17 +1,7 @@
-export default function Input(InputType) {
+export default function Input({InputType}) {
     console.log(InputType)
-    if(InputType.type === "text")
-    {
-        return (
-            <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
-            type="text"></input>
-        )
-    }
-    else
-    {
-        return (
-            <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
-            type="number"></input>
-        )
-    }
+    return (
+        <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+        type={InputType}></input>
+    )
 }
