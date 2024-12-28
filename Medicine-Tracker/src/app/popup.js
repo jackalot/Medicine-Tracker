2b4082c6bfc0146567f96ef5f98f51464fe598d9
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 10:52:31 -0500

    added some styling to the popup.

--- Medicine-Tracker/src/app/popup.js
+++ Medicine-Tracker/src/app/popup.js
@@ -1,6 +1,8 @@
 export default function Popup() {
     return (
-        <div className="absolute z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-          THIS OVERLAPS ALL  </div>
+        <div className="absolute z-10 bg-red-500 border-radius: 0.25rem w-1/5 h-1/5 border-style: solid border-color: black border-width: 8px" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          THIS OVERLAPS ALL  
+          <div></div>
+          </div>
     )
 }
