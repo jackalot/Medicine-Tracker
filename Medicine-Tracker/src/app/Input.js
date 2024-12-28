e04b722254d5f1c2f50265073120e7cffea47507
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 08:53:43 -0500

    removed console.log

--- Medicine-Tracker/src/app/Input.js
+++ Medicine-Tracker/src/app/Input.js
@@ -1,5 +1,4 @@
 export default function Input({InputType, changeParentState}) {
-    console.log(InputType)
     return (
         <input className="border-gray-300 text-white-600 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
         type={InputType}
