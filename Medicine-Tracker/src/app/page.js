105a91bda35f52be5ce7600818789ea3a5359225
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-28 09:30:29 -0500

    added 3 variables and logs

--- Medicine-Tracker/src/app/page.js
+++ Medicine-Tracker/src/app/page.js
@@ -37,6 +37,12 @@ export default function Home() {
       }
     }
      */
+    let totalPills = TotalPillAmount;
+    let morningTaken = 0;
+    let nightTaken = 0;
+    console.log("TotalPills: " + totalPills);
+    console.log("MorningTaken: " + morningTaken);
+    console.log("NightTaken: " + nightTaken);
   }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
