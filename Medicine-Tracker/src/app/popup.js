43102b7eb06fece98a8fe0eb38316c46f02dafd5
Christopher Gonzalez <gonzalezc9999@gmail.com>
2024-12-29 12:04:24 -0500

    added a comment to note where buttons start

--- Medicine-Tracker/src/app/popup.js
+++ Medicine-Tracker/src/app/popup.js
@@ -68,18 +68,21 @@ export default function Popup() {
                 </div>
               </div>
             </div>
+            {/*===========================================
+              This is our buttons, notepad and Cancel for now
+          ================================================*/}
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
               >
-                Deactivate
+                Cancel
               </button>
               <button
                 type="button"
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
               >
-                Cancel
+                Notepad
               </button>
             </div>
           </div>
